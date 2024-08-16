10e83f935886c2ce2a70735c0e1c62654713b67e
Ascic, Maid <ascija111@gmail.com>
2024-08-16 07:33:48 +0200

    Chat message send, Profile pic styling fix

--- src/Components/Chat/ChatContent/ChatContent.js
+++ src/Components/Chat/ChatContent/ChatContent.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from "./ChatContent.module.scss";
 import { useSelector } from 'react-redux';
-import BasicModal from '../../Common/BasicModal/BasicModal';
+// import BasicModal from '../../Common/BasicModal/BasicModal';
 import { useDispatch } from 'react-redux';
-import { toggleShowChatInfo } from '../../../store/chatSlice';
+import CustomButton from '../../Common/Buttons/CustomButton';
 
 function ChatContent() {
   return (
-    <div>Content</div>
+    // Chat window content with message box and message input, no header
+    <div className={ styles.mainChatContainer }>
+      <div className={ styles.chatContent }>
+      </div>
+      <div className={ styles.messageBoxContainer }>
+        <div className={ styles.inputContainer }>
+          <textarea placeholder="Type a message" />
+        </div>
+        <div className={ styles.buttonContainer }>
+          <CustomButton buttonText="Send" buttonSize="sm" buttonType="filled" />
+        </div>
+
+      </div>
+    </div>
   );
 }
 
