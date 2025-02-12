5c4b58601374947a6f146614bd66771fe104cc5d
Ascic, Maid <ascija111@gmail.com>
2025-02-12 22:35:57 +0100

    Chat Item props

--- src/Components/Chat/ChatSidebar/ChatSidebar.js
+++ src/Components/Chat/ChatSidebar/ChatSidebar.js
@@ -6,7 +6,7 @@ import { MESSAGE_STATUS } from 'src/Helpers/Constants';
 export default function ChatSidebar() {
   return (
     <div className={ styles.main }>
-      <ChatItem deliveryStatus={MESSAGE_STATUS.SENT}/>
+      <ChatItem deliveryStatus={MESSAGE_STATUS.SENT} email="mascija111@gmail.com"/>
     </div>
   );
 }
