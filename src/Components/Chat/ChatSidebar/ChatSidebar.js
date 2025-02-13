cea5b076a16bccaaae9bf01541d1999c686ab903
Ascic, Maid <ascija111@gmail.com>
2025-02-13 21:57:24 +0100

    Added contactUID to props

--- src/Components/Chat/ChatSidebar/ChatSidebar.js
+++ src/Components/Chat/ChatSidebar/ChatSidebar.js
@@ -13,13 +13,16 @@ export default function ChatSidebar() {
     <div className={ styles.main }>
       { allChats.map(chat => {
         var chatOwner = currentUser.uid === chat.user1ID ? chat.user2Email : chat.user1Email;
+        var contactUID = currentUser.uid === chat.user1ID ? chat.user2ID : chat.user1ID;
         var deliveryStatus = currentUser.uid === chat.lastMessageStatus.userID ? chat.lastMessageStatus.status : "";
         return <ChatItem
           deliveryStatus={ deliveryStatus }
           email={ chatOwner }
           timestamp={ chat.lastModified }
           chat={ chat }
-          key={ chat.id } />;
+          key={ chat.id }
+          contactUID={ contactUID }
+          />;
 
       }) }
     </div>
